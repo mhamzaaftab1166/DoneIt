8db2199ccba008617f7aa24dcae43ebb2cfcaf2d
Hamza Aftab <bhattihamza0099@gmail.com>
2023-08-09 15:08:24 +0500

    improve welcome tagline

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -7,6 +7,7 @@ function WelcomeScreen(props) {
   return (
     <>
       <ImageBackground
+        blurRadius={8}
         style={styles.background}
         source={require("../assets/background.jpg")}
       >
@@ -15,7 +16,7 @@ function WelcomeScreen(props) {
             style={styles.logo}
             source={require("../assets/logo-red.png")}
           />
-          <Text>Sell What You Want!!</Text>
+          <Text style={styles.tagline}>Sell What You Want!!</Text>
         </View>
         <View style={styles.buttonContainner}>
           <AppButton title={"Login"}></AppButton>
@@ -39,6 +40,11 @@ const styles = StyleSheet.create({
   logo: {
     width: 100,
     height: 100,
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
   buttonContainner: {
     padding: 20,
