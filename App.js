5843b2f32e2e08c25744353f54ab9a668af2749d
Hamza Aftab <bhattihamza0099@gmail.com>
2023-08-13 16:40:57 +0500

    implement reuseable icon component

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
+import SafeScreen from "./app/components/SafeScreen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
   return (
-    <GestureHandlerRootView>
-      <MessagesScreen />
-    </GestureHandlerRootView>
+    <SafeScreen>
+      <Icon
+        name={"email"}
+        size={50}
+        backgroundColor="red"
+        iconColor="white"
+      ></Icon>
+    </SafeScreen>
+    // <GestureHandlerRootView>
+    //   <MessagesScreen />
+    // </GestureHandlerRootView>
   );
 }
