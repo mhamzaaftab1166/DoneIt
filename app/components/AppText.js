871ff8fc3bc29d529cc3ea301dd5424cbbe4c620
Hamza Aftab <bhattihamza0099@gmail.com>
2023-08-14 17:45:57 +0500

    Extracted default styles

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
-
+import { Text } from "react-native";
+import defaultStyles from "../config/styles";
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
 
-const styles = StyleSheet.create({
-  text: {
-    fontWeight: "400",
-    textTransform: "capitalize",
-    ...Platform.select({
-      ios: {
-        fontSize: 20,
-        fontFamily: "Avenir",
-      },
-      android: {
-        fontSize: 20,
-        fontFamily: "Roboto",
-      },
-    }),
-  },
-});
+// const styles = StyleSheet.create({
+//   text: {
+//     fontWeight: "400",
+//     textTransform: "capitalize",
+//     ...Platform.select({
+//       ios: {
+//         fontSize: 20,
+//         fontFamily: "Avenir",
+//       },
+//       android: {
+//         fontSize: 20,
+//         fontFamily: "Roboto",
+//       },
+//     }),
+//   },
+// });
 
 export default AppText;
--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -2,6 +2,7 @@ import React from "react";
 import { TextInput, View, StyleSheet, Platform } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../config/colors";
+import defaultStyles from "../config/styles";
 function AppTextInput({ icon, ...otherProps }) {
   return (
     <View style={styles.container}>
@@ -9,17 +10,17 @@ function AppTextInput({ icon, ...otherProps }) {
         <MaterialCommunityIcons
           name={icon}
           size={20}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
           style={styles.icon}
         />
       )}
-      <TextInput style={styles.textInput} {...otherProps} />
+      <TextInput style={defaultStyles.text} {...otherProps} />
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
     width: "100%",
@@ -29,11 +30,6 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
-  },
-  textInput: {
-    fontSize: 19,
-    fontFamily: Platform.OS === "ios" ? "Avenir" : "Roboto",
-    color: colors.dark,
   },
 });
 export default AppTextInput;
