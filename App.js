2e8bfbc1e8d061b3df957b21c4bd8b1c84a7d3a3
Hamza Aftab <bhattihamza0099@gmail.com>
2023-08-14 17:27:52 +0500

    inplement text input component

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
+import { Text, TextInput, View } from "react-native";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import SafeScreen from "./app/components/SafeScreen";
+import { useState } from "react";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen></ListingsScreen>;
+  const [name, setName] = useState("");
+  return (
+    <SafeScreen>
+      <AppTextInput placeholder={"Username"} icon={"email"} />
+    </SafeScreen>
+  );
 }
