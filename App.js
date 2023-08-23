77a11957162ddcfee46618a0edb7c97fc0d8629e
Hamza Aftab <bhattihamza0099@gmail.com>
2023-08-23 17:14:09 +0500

    handled image touched

--- App.js
+++ App.js
@@ -1,32 +1,26 @@
 import React, { useEffect, useState } from "react";
 import SafeScreen from "./app/components/SafeScreen";
 import * as ImagePicker from "expo-image-picker";
-import { Button, Image } from "react-native";
 import ImageInput from "./app/components/ImageInput";
+
 function App(props) {
   const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert("Yoy need to enable permissons for accessing library!");
   };
+
   useEffect(() => {
     requestPermission();
   }, []);
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.canceled) setImageUri(result.assets[0].uri);
-    } catch (error) {
-      console.log("error reading an image", error);
-    }
-  };
+
   return (
     <SafeScreen>
-      <Button title="Select Image" onPress={selectImage}></Button>
-      {imageUri && (
-        <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-      )}
-      <ImageInput imageUri={imageUri} />
+      <ImageInput
+        imageUri={imageUri}
+        onSelectImage={(uri) => setImageUri(uri)}
+      />
     </SafeScreen>
   );
 }
--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,15 +1,50 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
 import colors from "../config/colors";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-function ImageInput({ imageUri }) {
+import * as ImagePicker from "expo-image-picker";
+
+function ImageInput({ imageUri, onSelectImage }) {
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert("DELETE", "Are you sure to delete image?", [
+        { text: "Yes", onPress: () => onSelectImage(null) },
+        { text: "No" },
+      ]);
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.canceled) onSelectImage(result.assets[0].uri);
+    } catch (error) {
+      console.log("error reading an image", error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {!imageUri && (
-        <MaterialCommunityIcons size={40} name="camera" color={colors.medium} />
-      )}
-      {imageUri && <Image style={styles.image} source={{ uri: imageUri }} />}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            size={40}
+            name="camera"
+            color={colors.medium}
+          />
+        )}
+        {imageUri && <Image style={styles.image} source={{ uri: imageUri }} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 const styles = StyleSheet.create({
