fd7d21f6f3172922d5d7527fe74afdb4e432d851
Hamza Aftab <bhattihamza0099@gmail.com>
2023-08-29 15:14:56 +0500

    customized theme

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import NavigationTheme from "./app/navigation/NavigationTheme";
 const Tweets = ({ navigation }) => {
   return (
     <SafeScreen>
@@ -82,7 +83,7 @@ const TabNavigator = () => (
 );
 function App(props) {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={NavigationTheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
