78940b61a4bf2d14ef4bd3ae5e8b4c0c38f05b27
Hamza Aftab <bhattihamza0099@gmail.com>
2023-08-30 16:41:26 +0500

    fetched litings with api layer

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,30 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SafeScreen from "../components/SafeScreen";
 import { FlatList, StyleSheet } from "react-native";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
-const listings = [
-  {
-    id: 1,
-    title: "Red Jacket For Sale",
-    price: 40,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-  {
-    id: 3,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-];
+import litingsApi from "../api/listings";
+
 function ListingsScreen({ navigation }) {
+  const [listings, setLitings] = useState([]);
+  useEffect(() => {
+    litingsApi.getListings().then((response) => setLitings(response.data));
+  }, []);
   return (
     <SafeScreen style={styles.screen}>
       <FlatList
@@ -34,7 +20,7 @@ function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={"$" + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
