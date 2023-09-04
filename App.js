c6e8404ff8d12e0efb53d410e5a54bcbc814ad5c
Hamza Aftab <bhattihamza0099@gmail.com>
2023-09-04 15:57:19 +0500

    implemented cache layer

--- App.js
+++ App.js
@@ -1,116 +1,19 @@
 import React from "react";
-import SafeScreen from "./app/components/SafeScreen";
-import { Button, Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import AuthNavigator from "./app/navigation/AuthNavigator";
-import NavigationTheme from "./app/navigation/navigationTheme";
-import AppNavigator from "./app/navigation/AppNavigator";
-// const Tweets = ({ navigation }) => {
-//   return (
-//     <SafeScreen>
-//       <Text>Tweets list</Text>
-//       <Button
-//         title="view tweet detail"
-//         onPress={() => navigation.navigate("TweetDetail", { id: 12 })}
-//       />
-//     </SafeScreen>
-//   );
-// };
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// const TweetDetail = ({ route }) => {
-//   return (
-//     <SafeScreen>
-//       <Text>Tweet Detail {route.params.id}</Text>
-//     </SafeScreen>
-//   );
-// };
-
-// const Stack = createStackNavigator();
-// const StackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerStyle: { backgroundColor: "dodgerblue" },
-//         headerTintColor: "white",
-//         headerTitleAlign: "center",
-//       }}
-//     >
-//       <Stack.Screen
-//         options={{
-//           headerStyle: { backgroundColor: "tomato" },
-//           headerTintColor: "white",
-//         }}
-//         name="Tweets"
-//         component={Tweets}
-//       />
-//       <Stack.Screen
-//         options={{ title: "lora mera" }}
-//         name="TweetDetail"
-//         component={TweetDetail}
-//       />
-//     </Stack.Navigator>
-//   );
-// };
-
-// const Account = () => (
-//   <SafeScreen>
-//     <Text>accout</Text>
-//   </SafeScreen>
-// );
-// const Tab = createBottomTabNavigator();
-// const TabNavigator = () => (
-//   <Tab.Navigator
-//   // tabBarOptions={{
-//   //   activeBackgroundColor: "tomato",
-//   //   activeTintColor: "white",
-//   //   inactiveBackgroundColor: "#eee",
-//   //   inactiveTintColor: "black",
-//   // }}
-//   >
-//     <Tab.Screen
-// options={{
-//   tabBarIcon: ({ size, color }) => (
-//     <MaterialCommunityIcons name="home" color={color} size={size} />
-//   ),
-// }}
-//       name="Feed"
-//       component={StackNavigator}
-//     />
-//     <Tab.Screen name="Account" component={Account} />
-//   </Tab.Navigator>
-// );
 function App(props) {
-  return (
-    <NavigationContainer theme={NavigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  const demo = async () => {
+    try {
+      // await AsyncStorage.setItem("person", JSON.stringify({ name: "hamza" }));
+      const value = await AsyncStorage.getItem("person");
+      const peron = JSON.parse(value);
+      console.log(peron);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  demo();
+  return null;
 }
 
 export default App;
-
-// import React, { useEffect, useState } from "react";
-// import SafeScreen from "./app/components/SafeScreen";
-// import * as ImagePicker from "expo-image-picker";
-// import ImageInput from "./app/components/ImageInput";
-// import ImageInputList from "./app/components/ImageInputList";
-// import ListingEditScreen from "./app/screens/ListingEditScreen";
-
-// function App(props) {
-//   const [imageUris, setImageUris] = useState([]);
-
-//   const requestPermission = async () => {
-//     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-//     if (!granted) alert("Yoy need to enable permissons for accessing library!");
-//   };
-
-//   useEffect(() => {
-//     requestPermission();
-//   }, []);
-
-//   return <ListingEditScreen />;
-// }
-// export default App;
