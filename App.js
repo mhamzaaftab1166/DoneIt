3df8b6ab7acf5dfea4f262d0009be98a97b7f25c
Hamza Aftab <bhattihamza0099@gmail.com>
2023-09-05 17:01:19 +0500

    controlled splash screen

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import AuthNavigator from "./app/navigation/AuthNavigator";
 import AppNavigator from "./app/navigation/AppNavigator";
 import NavigationTheme from "./app/navigation/navigationTheme";
 import OfflineNotice from "./app/components/OfflineNotice";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 import jwtDecode from "jwt-decode";
 
+import * as SplashScreen from "expo-splash-screen";
+
 function App(props) {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const authToken = await authStorage.getToken();
@@ -18,12 +21,33 @@ function App(props) {
   };
 
   useEffect(() => {
-    restoreToken();
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await restoreToken();
+      } catch (error) {
+        console.log("Error loading app", error);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
+
+  const onNavigationContainerReady = useCallback(async () => {
+    if (isReady) await SplashScreen.hideAsync();
+  }, [isReady]);
+
+  if (!isReady) return null;
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
-      <NavigationContainer theme={NavigationTheme}>
+      <NavigationContainer
+        theme={NavigationTheme}
+        onReady={onNavigationContainerReady}
+      >
         {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </AuthContext.Provider>
